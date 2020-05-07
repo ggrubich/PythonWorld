68689974fe1ae57259fbd1905062e03cb8e6d522
ggrubich <g.grubich.898@studms.ug.edu.pl>
2020-05-07 22:58:16 +0200

    Remove accessors for World's internal variables

--- World.py
+++ World.py
@@ -5,53 +5,17 @@ from Organisms.Plant import Plant
 class World(object):
 
     def __init__(self, worldX, worldY):
-        self.__worldX = worldX
-        self.__worldY = worldY
-        self.__turn = 0
-        self.__organisms = []
-        self.__newOrganisms = []
-        self.__separator = ' '
-
-    @property
-    def worldX(self):
-        return self.__worldX
-
-    @property
-    def worldY(self):
-        return self.__worldY
-
-    @property
-    def turn(self):
-        return self.__turn
-
-    @turn.setter
-    def turn(self, value):
-        self.__turn = value
-
-    @property
-    def organisms(self):
-        return self.__organisms
-
-    @organisms.setter
-    def organisms(self, value):
-        self.__organisms = value
-
-    @property
-    def newOrganisms(self):
-        return self.__newOrganisms
-
-    @newOrganisms.setter
-    def newOrganisms(self, value):
-        self.__newOrganisms = value
-
-    @property
-    def separator(self):
-        return self.__separator
+        self._worldX = worldX
+        self._worldY = worldY
+        self._turn = 0
+        self._organisms = []
+        self._newOrganisms = []
+        self._separator = ' '
 
     def makeTurn(self):
         actions = []
 
-        queue = self.organisms.copy()
+        queue = self._organisms.copy()
         queue.sort(key=lambda o: o.initiative, reverse=True)
         for org in queue:
             if not org.frozen and self.positionOnBoard(org.position):
@@ -65,39 +29,39 @@ class World(object):
                         self.makeMove(a)
                     actions = []
 
-        for org in self.organisms:
+        for org in self._organisms:
             if not org.frozen:
                 org.liveLength -= 1
                 org.power += 1
             org.frozen = False
             if org.liveLength < 1:
                 print(str(org.__class__.__name__) + ': died of old age at: ' + str(org.position))
-        self.organisms = [o for o in self.organisms if o.liveLength > 0]
+        self._organisms = [o for o in self._organisms if o.liveLength > 0]
 
-        self.organisms.extend(self.newOrganisms)
-        self.newOrganisms = []
+        self._organisms.extend(self._newOrganisms)
+        self._newOrganisms = []
 
-        self.turn += 1
+        self._turn += 1
 
     def makeMove(self, action):
         print(action)
         action.run()
 
     def addOrganism(self, newOrganism):
-        self.organisms.append(newOrganism)
+        self._organisms.append(newOrganism)
 
     def scheduleOrganism(self, organism):
-        self.newOrganisms.append(organism)
+        self._newOrganisms.append(organism)
 
     def removeOrganism(self, organism):
-        self.organisms.remove(organism)
+        self._organisms.remove(organism)
         organism.position = Position.invalid()
 
     def positionOnBoard(self, position):
-        return position.x >= 0 and position.y >= 0 and position.x < self.worldX and position.y < self.worldY
+        return position.x >= 0 and position.y >= 0 and position.x < self._worldX and position.y < self._worldY
 
     def getOrganismFromPosition(self, position):
-        for org in self.organisms:
+        for org in self._organisms:
             if org.position == position:
                 return org
         return None
@@ -140,13 +104,13 @@ class World(object):
         return result
 
     def __str__(self):
-        result = '\nturn: ' + str(self.__turn) + '\n'
-        for wY in range(0, self.worldY):
-            for wX in range(0, self.worldX):
+        result = '\nturn: ' + str(self._turn) + '\n'
+        for wY in range(0, self._worldY):
+            for wX in range(0, self._worldX):
                 org = self.getOrganismFromPosition(Position(wX, wY))
                 if org:
                     result += str(org.sign)
                 else:
-                    result += self.separator
+                    result += self._separator
             result += '\n'
         return result
