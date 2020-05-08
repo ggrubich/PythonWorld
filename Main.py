a74e3c0748ccabb67fe78904d16a7321e3f226aa
ggrubich <g.grubich.898@studms.ug.edu.pl>
2020-05-08 17:37:46 +0200

    Allow users to add organisms during the simulation

--- Main.py
+++ Main.py
@@ -9,6 +9,52 @@ from Organisms.Antelope import Antelope
 from Organisms.Alien import Alien
 import os
 
+knownOrganisms = [
+    Alien,
+    Antelope,
+    Sheep,
+    Wolf,
+    Dandelion,
+    Grass,
+    Toadstool,
+]
+
+def readInt():
+    choice = input('> ')
+    try:
+        n = int(choice)
+        return n
+    except ValueError:
+        return None
+
+def readClass():
+    print('Select organism to add')
+    for i, org in enumerate(knownOrganisms):
+        print('{}) {}'.format(i, org.name))
+    while True:
+        idx = readInt()
+        if idx is None or idx not in range(len(knownOrganisms)):
+            print('Not a valid index')
+        else:
+            break
+    print('Selected {}'.format(knownOrganisms[idx].name))
+    return knownOrganisms[idx]
+
+
+def readCoord(kind, stop):
+    print('Choose the {} coordinate'.format(kind))
+    while True:
+        n = readInt()
+        if n is None or n not in range(stop):
+            print('Not a valid {} coordinate'.format(kind))
+        else:
+            return n
+
+def readPosition(width, height):
+    x = readCoord('x', width)
+    y = readCoord('y', height)
+    return Position(x, y)
+
 def clear():
     if os.name == 'nt':
         os.system('cls')
@@ -41,7 +87,15 @@ if __name__ == '__main__':
     print(pyWorld)
 
     for _ in range(0, 100):
-        input('')
-        clear()
-        pyWorld.makeTurn()
-        print(pyWorld)
+        print('Press `Enter` for next turn, `a` to add an organism')
+        choice = input('> ')
+        if choice == 'a':
+            cls = readClass()
+            pos = readPosition(pyWorld.width, pyWorld.height)
+            pyWorld.addOrganism(cls(pos, pyWorld))
+            clear()
+            print(pyWorld)
+        else:
+            clear()
+            pyWorld.makeTurn()
+            print(pyWorld)
