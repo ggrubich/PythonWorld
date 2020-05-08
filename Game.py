9a6616bbe7e69f6f0a9dcf7b3ec5963201daf21f
ggrubich <g.grubich.898@studms.ug.edu.pl>
2020-05-08 18:39:41 +0200

    Move the main loop to Game class

---
+++ Game.py
@@ -0,0 +1,82 @@
+from World import World
+from Position import Position
+
+import os
+
+
+class Game(object):
+
+    def __init__(self, width, height, knownOrganisms, startingOrganisms):
+        self._knownOrganisms = knownOrganisms
+        self._world = World(width, height)
+        for cls, pos in startingOrganisms:
+            self._world.addOrganism(cls(pos, self._world))
+
+    def readInt(self):
+        choice = input('> ')
+        try:
+            n = int(choice)
+            return n
+        except ValueError:
+            return None
+
+    def readClass(self):
+        print('Select organism to add')
+        for i, org in enumerate(self._knownOrganisms):
+            print('{}) {}'.format(i, org.name))
+        while True:
+            idx = self.readInt()
+            if idx is None or idx not in range(len(self._knownOrganisms)):
+                print('Not a valid index')
+            else:
+                break
+        print('Selected {}'.format(self._knownOrganisms[idx].name))
+        return self._knownOrganisms[idx]
+
+
+    def readCoord(self, kind, stop):
+        print('Choose the {} coordinate'.format(kind))
+        while True:
+            n = self.readInt()
+            if n is None or n not in range(stop):
+                print('Not a valid {} coordinate'.format(kind))
+            else:
+                return n
+
+    def readPosition(self):
+        while True:
+            x = self.readCoord('x', self._world.width)
+            y = self.readCoord('y', self._world.height)
+            pos = Position(x, y)
+            if self._world.getOrganismFromPosition(pos) is not None:
+                print('Position already taken')
+            else:
+                return pos
+
+    def readOrganism(self):
+        cls = self.readClass()
+        pos = self.readPosition()
+        return cls(pos, self._world)
+
+    def clear(self):
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            print('\x1b[2J \x1b[H', end='')
+
+    def run(self):
+        self.clear()
+        print(self._world)
+
+        for _ in range(0, 100):
+            print('Press `Enter` for next turn, `a` to add an organism')
+            choice = input('> ')
+            if choice == 'a':
+                org = self.readOrganism()
+                self._world.addOrganism(org)
+                self.clear()
+                print(self._world)
+            else:
+                self.clear()
+                self._world.makeTurn()
+                print(self._world)
--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from World import World
+from Game import Game
 from Position import Position
 from Organisms.Grass import Grass
 from Organisms.Sheep import Sheep
@@ -7,95 +7,28 @@ from Organisms.Wolf import Wolf
 from Organisms.Toadstool import Toadstool
 from Organisms.Antelope import Antelope
 from Organisms.Alien import Alien
-import os
 
-knownOrganisms = [
-    Alien,
-    Antelope,
-    Sheep,
-    Wolf,
-    Dandelion,
-    Grass,
-    Toadstool,
-]
-
-def readInt():
-    choice = input('> ')
-    try:
-        n = int(choice)
-        return n
-    except ValueError:
-        return None
-
-def readClass():
-    print('Select organism to add')
-    for i, org in enumerate(knownOrganisms):
-        print('{}) {}'.format(i, org.name))
-    while True:
-        idx = readInt()
-        if idx is None or idx not in range(len(knownOrganisms)):
-            print('Not a valid index')
-        else:
-            break
-    print('Selected {}'.format(knownOrganisms[idx].name))
-    return knownOrganisms[idx]
-
-
-def readCoord(kind, stop):
-    print('Choose the {} coordinate'.format(kind))
-    while True:
-        n = readInt()
-        if n is None or n not in range(stop):
-            print('Not a valid {} coordinate'.format(kind))
-        else:
-            return n
-
-def readPosition(width, height):
-    x = readCoord('x', width)
-    y = readCoord('y', height)
-    return Position(x, y)
-
-def clear():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        print('\x1b[2J \x1b[H', end='')
+def main():
+    knownOrganisms = [
+        Alien,
+        Antelope,
+        Sheep,
+        Wolf,
+        Dandelion,
+        Grass,
+        Toadstool,
+    ]
+    startingOrganisms = [
+        (Grass, Position(4, 0)),
+        (Sheep, Position(0, 0)),
+        (Dandelion, Position(0, 4)),
+        (Wolf, Position(7, 7)),
+        (Toadstool, Position(4, 4)),
+        (Antelope, Position(6, 7)),
+        (Alien, Position(6, 6)),
+    ]
+    game = Game(8, 8, knownOrganisms, startingOrganisms)
+    game.run()
 
 if __name__ == '__main__':
-    pyWorld = World(8, 8)
-
-    newOrg = Grass(Position(4, 0), pyWorld)
-    pyWorld.addOrganism(newOrg)
-
-    newOrg = Sheep(Position(0, 0), pyWorld)
-    pyWorld.addOrganism(newOrg)
-
-    newOrg = Dandelion(Position(0, 4), pyWorld)
-    pyWorld.addOrganism(newOrg)
-    
-    newOrg = Wolf(Position(7, 7), pyWorld)
-    pyWorld.addOrganism(newOrg)
-
-    newOrg = Toadstool(Position(4, 4), pyWorld)
-    pyWorld.addOrganism(newOrg)
-
-    pyWorld.addOrganism(Antelope(Position(6, 7), pyWorld))
-
-    pyWorld.addOrganism(Alien(Position(6, 6), pyWorld))
-
-    clear()
-    print(pyWorld)
-
-    for _ in range(0, 100):
-        print('Press `Enter` for next turn, `a` to add an organism')
-        choice = input('> ')
-        if choice == 'a':
-            cls = readClass()
-            pos = readPosition(pyWorld.width, pyWorld.height)
-            pyWorld.addOrganism(cls(pos, pyWorld))
-            clear()
-            print(pyWorld)
-        else:
-            clear()
-            pyWorld.makeTurn()
-            print(pyWorld)
+    main()
